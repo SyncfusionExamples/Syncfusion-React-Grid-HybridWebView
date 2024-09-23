33394e1460352a965f374bbacb3e645f2f6cf361
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2024-09-23 18:00:16 +0530

    Added example for the Syncfusion react components within the HybridWebView

---
+++ SyncfusionMauiHybridWebView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SyncfusionMauiHybridWebView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
